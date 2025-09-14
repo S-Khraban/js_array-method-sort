c571e695963c3537d3acae0b7d22a6e24cb28778
S-Khraban <s.hraban@i.ua>
2025-09-14 09:27:17 +0300

    _fixed all the review comments_

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -2,22 +2,28 @@
 
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
-    const cmp =
-      compareFunction ||
-      function (a, b) {
-        const A = String(a);
-        const B = String(b);
+    const defaultComparator = (a, b) => {
+      const A = String(a);
+      const B = String(b);
 
-        if (A < B) {
-          return -1;
-        }
+      if (A < B) {
+        return -1;
+      }
 
-        if (A > B) {
-          return 1;
-        }
+      if (A > B) {
+        return 1;
+      }
 
-        return 0;
-      };
+      return 0;
+    };
+
+    let cmp;
+
+    if (typeof compareFunction === 'function') {
+      cmp = compareFunction;
+    } else {
+      cmp = defaultComparator;
+    }
 
     for (let i = 0; i < this.length - 1; i++) {
       for (let j = 0; j < this.length - 1 - i; j++) {
